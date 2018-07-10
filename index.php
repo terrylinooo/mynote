027998c14ed23e9d74879a49165263f5afbf1089
Terry Lin <terrylin.developer@gmail.com>
2018-07-10 18:13:05 +0800

    Update homepage

--- index.php
+++ index.php
@@ -14,13 +14,45 @@ get_header(); ?>
 
 	<main role="main">
 
+		<div class="section-intro d-flex align-items-center">
+			
+		
+			<div class="container px-responsive">
+
+				<div class="d-md-flex align-items-center">
+					
+						<div class="col-12 col-md-7 text-center text-md-left" style="min-height: 100%; overflow: hidden" >
+
+
+							<h1 class="mb-3"><?php echo get_bloginfo( 'name' ); ?></h1>
+							<p class="mb4 desc-text"><?php echo html_entity_decode( get_bloginfo( 'description') ); ?></p>
+							
+
+
+						</div>
+						<div class="col-12 col-md-5">
+
+							<p class="p-5"><?php home_category_labels(); ?></p>
+						</div>
+
+				
+				</div>
+
+			</div>
+
+
+		</div>
+
+		<div class="container">
 		<section>
 
-			<h1><?php esc_html_e( 'Latest Posts', 'githuber' ); ?></h1>
 			<?php get_template_part( 'loop' ); ?>
+
 			<?php get_template_part( 'pagination' ); ?>
 
 		</section>
+
+		</div>
 
 	</main>
 
