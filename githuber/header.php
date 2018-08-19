d51e4744ffe74fffff551f08159f71f2f60bd7c1
Terry Lin <terrylin.developer@gmail.com>
2018-08-20 03:20:06 +0800

    Fix RWD and remove meta description in header.php

--- githuber/header.php
+++ githuber/header.php
@@ -17,7 +17,6 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="<?php bloginfo( 'description' ); ?>">
 
 <?php wp_head(); ?>
 
