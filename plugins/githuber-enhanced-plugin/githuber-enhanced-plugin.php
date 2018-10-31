2b7e4fd3c98c510bf1dc8efca4ea94b078550eec
terrylinooo <terrylin.developer@gmail.com>
2018-10-31 16:58:25 +0000

    Rename plugin name

---
+++ plugins/githuber-enhanced-plugin/githuber-enhanced-plugin.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Githuber Enhanced Plugin
+ *
+ * @author Terry Lin
+ * @link https://terryl.in/githuber
+ *
+ * @package Githuber
+ * @since 1.1.0
+ * @version 1.1.0
+ */
+
+/**
+ * Plugin Name:       Githuber Enhanced Plugin
+ * Plugin URI:        https://terryl.in/githuber
+ * Description:       A WordPress plugin that improves Githuber theme functionality.
+ * Version:           1.1.0
+ * Author:            Terry Lin
+ * Author URI:        https://terryl.in
+ * License:           GPL 3.0
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:       githuber-plugin
+ * Domain Path:       /languages
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'GITHUBER_PLUGIN_VERSION', '1.1.0' );
+define( 'GITHUBER_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+require_once GITHUBER_PLUGIN_DIR . 'inc/class-githuber-plugin.php';
+
+$gitbuber = new Githuber_Plugin();
+$gitbuber->init();
+
---
+++ plugins/githuber-enhanced-plugin/readme.txt
@@ -0,0 +1,32 @@
+=== Githuber Framework ===
+Contributors: terrylin
+Tags: githuber
+Requires at least: 4.7
+Tested up to: 4.7
+Stable tag: 4.7
+Requires PHP: 5.6
+License: GPLv3 or later
+License URI: https://www.gnu.org/licenses/gpl.html
+
+== Description ==
+
+A WordPress plugin that improves [Githuber theme](https://github.com/terrylinooo/githuber) functionality.
+This plugin will not uploaded to WordPress plugin SVN server, you need to install it manually.
+
+* Widget: Table of Content.
+* Post type: GitHub Repository.
+
+== Changelog ==
+
+= 1.0.0
+
+* First release.
+
+== To do ==
+
+* Widget: Post status dashboard.
+* Plugin: Github oAuth + Only allow Github member to leave comments.
+* Plugin: Sync content to github page
+* Plugin: Markdown editor + preview.
+
+
